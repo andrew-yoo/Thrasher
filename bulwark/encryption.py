9d882a518f2b3953554bc2d0c9e38ca72b3e7f5d
Andrew Yoo <199436365+andrew-yoo@users.noreply.github.com>
2026-07-02 15:26:26 -0400

    Start the encryption file

---
+++ bulwark/encryption.py
@@ -0,0 +1,23 @@
+import nacl
+from shared import Cipher
+
+AEGIS_NONCE = 256
+XCHACHA_NONCE = 192
+
+# PyNaCl has no high-level bindings for AEGIS
+
+def encrypt(cipher_class):
+    e = nacl.bindings.crypto_aead_aegis256_encrypt
+    if cipher_class.overkill:
+        pass
+
+    else:
+        pass
+
+def decrypt(cipher_class):
+    d = nacl.bindings.crypto_aead_aegis256_decrypt
+    if cipher_class.overkill:
+        pass
+
+    else:
+        pass
--- bulwark/shared.py
+++ bulwark/shared.py
@@ -1,6 +1,14 @@
-class Argon:
-    def __init__(self, salt, password, overkill, key=None):
+class KDF:
+    def __init__(self, salt, overkill, password, key=None):
         self.salt = salt
-        self.password = password
         self.overkill = overkill
+        self.password = password
         self.key = key
+
+class Cipher:
+    def __init__(self, nonce, key, overkill, plaintext=None, ciphertext=None):
+        self.nonce = nonce
+        self.key = key
+        self.overkill = overkill
+        self.plaintext = plaintext
+        self.ciphertext = ciphertext
