999ef48ebd443a7f52a74d13e4c5800e3063f99c
Andrew Yoo <199436365+andrew-yoo@users.noreply.github.com>
2026-08-07 02:00:56 -0400

    Remove overkill mode

--- thrasher/kdf.py
+++ thrasher/kdf.py
@@ -2,31 +2,20 @@ from cryptography.hazmat.primitives.kdf.argon2 import Argon2id
 import nacl.hash
 import nacl.encoding
 
-from .shared import Header
-
-NORMAL_SETTINGS = {
-    "m": 1_048_576,
-    "t": 4,
-    "p": 4,
-}
-OVERKILL_SETTINGS = {
+SETTINGS = {
     "m": 2_097_152,
     "t": 8,
-    "p": 8,
+    "p": 4,
 }
 
 
 def derive_master(kdf_class):
-    if kdf_class.mode == Header.OVERKILL:
-        settings = OVERKILL_SETTINGS
-    else:
-        settings = NORMAL_SETTINGS
     kdf = Argon2id(
         salt=kdf_class.salt,
         length=64,
-        iterations=settings["t"],
-        lanes=settings["p"],
-        memory_cost=settings["m"],
+        iterations=SETTINGS["t"],
+        lanes=SETTINGS["p"],
+        memory_cost=SETTINGS["m"],
         ad=None,
         secret=None,
     )
@@ -41,13 +30,5 @@ def derive_aegis_key(master_key):
     return _blake2b(master_key, 32, b"\x01")
 
 
-def derive_xchacha_key(master_key):
-    return _blake2b(master_key, 32, b"\x02")
-
-
 def derive_aegis_nonce(master_key):
     return _blake2b(master_key, 32, b"\x03")
-
-
-def derive_xchacha_nonce(master_key):
-    return _blake2b(master_key, 24, b"\x04")
