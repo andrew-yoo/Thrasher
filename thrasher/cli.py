7739f3a07c0500481a44608b19ca8e16e91d99ef
Andrew Yoo <199436365+andrew-yoo@users.noreply.github.com>
2026-08-08 20:37:48 -0400

    Restrict overwrite to decryption

--- thrasher/cli.py
+++ thrasher/cli.py
@@ -8,11 +8,15 @@ from .main import encrypt, decrypt
 def main():
     parser = argparse.ArgumentParser(description="Thrasher")
     parser.add_argument("file", help="file to encrypt or decrypt")
-    parser.add_argument("-w", "--overwrite", action="store_true", help="overwrite the input file")
+    parser.add_argument("-w", "--overwrite", action="store_true", help="overwrite the input file on decryption")
     args = parser.parse_args()
 
     try:
         if not args.file.endswith(".thrash"):
+            if args.overwrite:
+                print("-w/--overwrite is only supported when decrypting", file=sys.stderr)
+                sys.exit(1)
+
             password = getpass.getpass("Password: ").encode()
 
             if password == b"":
