3981a44dc9cee5bd55f3d31e48b544a54f50b5b3
Andrew Yoo <199436365+andrew-yoo@users.noreply.github.com>
2026-08-10 18:24:13 -0400

    Fix broken kdf test

--- tests/test_kdf.py
+++ tests/test_kdf.py
@@ -7,6 +7,6 @@ PASSWORD = b"password"
 MASTER_KEY = bytes.fromhex("ba8305a5d4306ec3527b6ac2c13eecb34ea5f9a209b766c9e7afa7627ee67703")
 
 
-def test_derive_master():
+def test_derive_key():
     kdf_obj = KDF(salt=SALT, password=PASSWORD)
-    assert kdf.derive_master(kdf_obj) == MASTER_KEY
+    assert kdf.derive_key(kdf_obj) == MASTER_KEY
